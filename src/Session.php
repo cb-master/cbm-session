68c2c5ff5d61c86fb26ef47c3a00e70d0cc9ec4a
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-24 18:12:22 +0600

    session class added

--- src/Session.php
+++ src/Session.php
@@ -1,1 +1,20 @@
 <?php
+/**
+ * APP Name:        Laika Session Model
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */
+
+// Namespace
+namespace CBM\Session;
+
+class Session
+{
+    public static function set($name)
+    {
+        return $name;
+    }
+}
