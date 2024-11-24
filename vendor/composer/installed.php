08770655294453a66628a92ed755866ae87d4d76
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-24 18:05:55 +0600

    lock removed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'cb-master/cbm-session',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'a0ea4e7bbbe559970f4769fe82dccef347c2347c',
+        'reference' => 'bb91877c6e66725f7d21e98b6b5517cd6f432e2f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -24,7 +24,7 @@
         'cb-master/cbm-session' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'a0ea4e7bbbe559970f4769fe82dccef347c2347c',
+            'reference' => 'bb91877c6e66725f7d21e98b6b5517cd6f432e2f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
