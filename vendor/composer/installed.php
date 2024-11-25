c975da827cb17544e4adb618dd0f90cc17ed4089
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-26 00:34:10 +0600

    vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cb-master/cb-session',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b8eecadd8088947d614655be542c23635c9d9058',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cb-master/cb-session' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b8eecadd8088947d614655be542c23635c9d9058',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
